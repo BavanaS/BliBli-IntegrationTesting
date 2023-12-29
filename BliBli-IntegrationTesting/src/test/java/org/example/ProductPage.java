e0e92a2b0e4525c5766e162f26d22c9f1f42dc33
Bavana <112500726+BavanaS@users.noreply.github.com>
2023-12-29 09:55:44 +0530

    Update ProductPage.java

--- BliBli-IntegrationTesting/src/test/java/org/example/ProductPage.java
+++ BliBli-IntegrationTesting/src/test/java/org/example/ProductPage.java
@@ -2,11 +2,15 @@ package org.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.util.*;
 public class ProductPage
 {
-    public void selectProduct(WebDriver driver,String orderOfProduct,int noOfProduct) throws InterruptedException
+    public void selectProduct(WebDriver driver,String orderOfProduct,int noOfProduct, WebDriverWait wait) throws InterruptedException
     {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='blu-product__name'])["+orderOfProduct+"]")));
         driver.findElement(By.xpath("(//div[@class='blu-product__name'])["+orderOfProduct+"]")).click();
         List<String> tabs = new ArrayList<> (driver.getWindowHandles());
         System.out.println(tabs.size());
@@ -16,15 +20,16 @@ public class ProductPage
         int iteration=1;
         while(iteration<noOfProduct)
         {
-            Thread.sleep(2000);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@data-testid='bagRetailAddQuantityButton']")));
             driver.findElement(By.xpath("//button[@data-testid='bagRetailAddQuantityButton']")).click();
             iteration++;
         }
     }
 
-    public void checkout(WebDriver driver) throws InterruptedException
+    public void checkout(WebDriver driver, WebDriverWait wait) throws InterruptedException
     {
-        Thread.sleep(2000);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='blu-btn b-secondary btn-checkout']")));
+        //Thread.sleep(2000);
         driver.findElement(By.xpath("//button[@class='blu-btn b-secondary btn-checkout']")).click();
     }
 }
